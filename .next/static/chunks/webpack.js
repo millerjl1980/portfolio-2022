4e3fd5dedf0f315c7ad83afb10815170f41d9aed
Justin Miller <indymiller@gmail.com>
2022-03-06 12:04:53 -0500

    completed projects section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d422d7ec4c9829e8"; }
+/******/ 		__webpack_require__.h = function() { return "a2298f9675ea381c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.16823308347e0f58.hot-update.js
+++ .next/static/webpack/webpack.16823308347e0f58.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d5d316a320705b37"; }
+/******/ 	__webpack_require__.h = function() { return "3d521e668419394c"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
