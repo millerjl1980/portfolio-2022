a0d293c0f803c9c1dd5e4d0fdb45198d216fcdf5
Justin Miller <indymiller@gmail.com>
2022-03-06 13:44:07 -0500

    completed accomplishment component

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -2,18 +2,24 @@ import React from 'react';
 
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import { AccomplishmentData } from '../../constants/constants';
 
-const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
-];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionDivider />
+    <SectionTitle main>
+      Personal Accomplishments
+    </SectionTitle>
+    <Boxes>
+      {AccomplishmentData.map((card, index)=>(
+        <Box key={index}>
+          <BoxNum>{card.number}+</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
